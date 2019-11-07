643e117952433620c7e358d1270f8cb5245c6d84
Livius-O <Livius-O@users.noreply.github.com>
2019-11-07 09:45:35 -0400

    Comenze la parte de php

---
+++ Jeremy/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    
+</head>
+<body>    
+<header>
+
+</header>
+
+<table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Suplidor</th>
+        <th scope="col">Pedido</th>
+        <th scope="col">Cantidad</th>
+        <th scope="col">Costo</th>
+        <th scope="col">Monto</th>
+        <th scope="col">Fecha limite</th>
+        <th scope="col">Factura</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    
+    
+    </tbody>
+  </table>
+
+<footer>
+
+</footer>
+<script src="js/jquery.js"></script>
+<script src="/Jeremy/js/bootstrap.js"></script>
+</body>
+</html>

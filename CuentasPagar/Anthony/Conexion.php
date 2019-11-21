e96cc4fa6c7fe64489311e04b9b18145453db272
Livius-O <Livius-O@users.noreply.github.com>
2019-11-21 10:39:36 -0400

    lol

---
+++ CuentasPagar/Anthony/Conexion.php
@@ -0,0 +1,12 @@
+<!-- Aqui esta la conexion de mi documento php-->
+
+<?php
+/*
+La real conxion
+*/
+session_start();
+$con = mysqli_connect("localhost", "root","", "cuentas_pagar");
+if(isset($con)){
+ 
+}
+?>
---
+++ CuentasPagar/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+  <title>Document</title>
+
+  <script src="validar.js"></script>
+
+</head>
+
+<body>
+  <ul class="nav nav-tabs">
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Inicio</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Clientes</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Anthony/index.php">Suplidores</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="phpV5brA+\Cuentas_pagar2V3.php">Agregar pedidos</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="CxP%20jer/index.php">Cuentas Por Pagar</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Totales</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Usuarios</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Salir</a>
+    </li>
+  </ul>

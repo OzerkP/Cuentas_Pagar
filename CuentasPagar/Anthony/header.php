4135d7120d5560d53d42181de544ddeda152dbc3
Livius-O <Livius-O@users.noreply.github.com>
2019-11-29 11:18:37 -0400

    Cambios recientes

--- CuentasPagar/Anthony/header.php
+++ CuentasPagar/Anthony/header.php
@@ -10,6 +10,12 @@
    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="css2/simple-sidebar.css" rel="stylesheet">
+
     <title>Document</title>
    
     <script src="validar.js"></script>

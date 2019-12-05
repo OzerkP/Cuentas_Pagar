a44778afe6528afe015653f064d59782afcae469
Livius-O <Livius-O@users.noreply.github.com>
2019-12-05 10:17:47 -0400

    erfse

---
+++ Facturacion/config/db.php
@@ -0,0 +1,24 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'root');//Usuario de tu base de datos
+define('DB_PASS', '');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'simple_invoice');//Nombre de la base de datos
+ 
+/*Datos de la empresa*/
+define('NOMBRE_EMPRESA', 'SISTEMAS WEB LA');
+define('DIRECCION_EMPRESA', 'Santa Cruz Distrito nacional, calle#4 ');
+define('TELEFONO_EMPRESA', '1+(849) 797-7417');
+define('EMAIL_EMPRESA', 'infotep@gmail.com');
+define('TAX', '13');
+
+/*-------------------------------------------------
+Los datos por defecto del usuario administrador son:
+#usuario: admin
+#contraseña: admin
+Los datos de inicio de sesion podran ser modificados 
+desde el panel de control del sistema
+---------------------------------------------------*/
+
+
+?>

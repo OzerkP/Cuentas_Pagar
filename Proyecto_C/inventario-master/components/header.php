b979387f1d8b9bf6138e65d58e59e699d057b0c4
Livius-O <Livius-O@users.noreply.github.com>
2019-12-10 22:43:57 -0400

    MODIFICAION

--- Proyecto_C/inventario-master/components/header.php
+++ Proyecto_C/inventario-master/components/header.php
@@ -10,7 +10,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Simple Sidebar - Start Bootstrap Template</title>
+  <title>Facturacion</title>
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
--- Proyecto_C/inventario-master/components/sidebar.php
+++ Proyecto_C/inventario-master/components/sidebar.php
@@ -2,12 +2,12 @@
 
 <!-- Sidebar -->
 <div class="bg-light border-right" id="sidebar-wrapper">
-  <div class="sidebar-heading">Start Bootstrap </div>
+  <div class="sidebar-heading">Inventario/Facturacion</div>
   <div class="list-group list-group-flush">
-    <a href="index.php" class="list-group-item list-group-item-action bg-light">productos</a>
+    <a href="index.php" class="list-group-item list-group-item-action bg-light">Productos</a>
     <a href="form.php" class="list-group-item list-group-item-action bg-light">Agregar Producto</a>
     <a href="Update.php" class="list-group-item list-group-item-action bg-light">Actualizar</a>
-    <a href="facturas.php" class="list-group-item list-group-item-action bg-light">facturacion</a>
+    <a href="facturas.php" class="list-group-item list-group-item-action bg-light">Facturacion</a>
     <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
     <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
   </div>
@@ -18,7 +18,7 @@
 <div id="page-content-wrapper">
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-    <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+    <button class="btn btn-primary" id="menu-toggle">Menu</button>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -27,22 +27,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="../index.php">Home <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </a>
-          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
+        
+      
       </ul>
     </div>
   </nav>
